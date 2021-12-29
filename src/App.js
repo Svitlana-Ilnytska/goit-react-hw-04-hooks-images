93ddddb6c2ec45558f66e77307fd48bfbdec966e
Svitlana-Ilnytska <72188793+Svitlana-Ilnytska@users.noreply.github.com>
2021-12-29 23:37:50 +0200

    added some changes in App func

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Searchbar from "./components/Searchbar/Searchbar";
 import ImageGallery from "./components/ImageGallery/ImageGallery";
 import Spiner from "./components/Loader/Loader";
@@ -11,102 +11,229 @@ import * as api from "./services/api";
 
 import "./App.css";
 
+export default function App() {
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+  const [error, setError] = useState(null);
+  const [largeImageURL, setLargeImageURL] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const [loader, setLoader] = useState(false);
 
-export default class App extends Component {
-  state = {
-    images: [],
-    query: "",
-    page: 1,
-    error: null,
-    largeImageURL: "",
-    showModal: false,
-    loader: false,
+  // componentDidUpdate(prevProps, prevState) {
+  //   const prevQuery = prevState.query;
+  //   const nextQuery = this.state.query;
+
+  //   if (prevQuery !== nextQuery) {
+  //     this.fetchImages();
+  //   }
+  //   const prevPage = prevState.page;
+  //   const nextPage = this.state.page;
+  //   if (prevPage !== nextPage) {
+  //     window.scrollTo({
+  //       top: document.body.scrollHeight,
+  //       behavior: "smooth",
+  //     });
+  //   }
+  // }
+  const fetchImages = () => {
+    setLoader(true);
+
+    api
+      .fetchImages(query, page )
+      .then(
+        ({ hits }) => setImages((prevImage) => [...prevImage, ...hits]),
+        setPage((prevPage) => prevPage + 1)
+      )
+
+      .catch(
+        (error) => setError(error),
+        toast("Trouble. Something is wrong :(")
+      )
+
+      .finally(() => setLoader(false));
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    const prevQuery = prevState.query;
-    const nextQuery = this.state.query;
-
-    if (prevQuery !== nextQuery) {
-      this.fetchImages();
+  useEffect(() => {
+    if (!query) {
+      return;
     }
-    const prevPage = prevState.page;
-    const nextPage = this.state.page;
-    if (prevPage !== nextPage) {
+
+    fetchImages();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query]);
+
+  const nextPage = () => {
+    setPage((prevPage) => prevPage + 1);
+    if (nextPage !== page) {
       window.scrollTo({
         top: document.body.scrollHeight,
         behavior: "smooth",
       });
     }
-  }
-  onSearchQuery = (query) => {
-    this.setState({
-      images: [],
-      query: query,
-      page: 1,
-    });
   };
 
-  fetchImages = () => {
-    const { query, page } = this.state;
-    this.setState({ loader: true });
+  // const handleFormSubmit = (images) => {
+  //   setImages(images);
+  // };
 
-    api
-      .fetchImages(query, page)
-      .then(({ hits }) =>
-        this.setState((prevState) => ({
-          images: [...prevState.images, ...hits],
-          page: prevState.page + 1,
-        }))
-      )
+  const onSearchQuery = useCallback((query) => {
+    setQuery(query);
+    setPage(1);
+    setImages([]);
+  }, []);
 
-      .catch((error) => {
-        toast("Trouble. Something is wrong :(");
-        this.setState({ error });
-      })
-
-      .finally(() => this.setState({ loader: false }));
-  };
-  handleFormSubmit = (images) => {
-    this.setState({ images });
-  };
-
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
+  const toggleModal = () => {
+    setShowModal((showModal) => ({
       showModal: !showModal,
       largeImageURL: "",
     }));
   };
 
-  handleClickImage = (largeImageURL) => {
-    this.setState({
-      largeImageURL: largeImageURL,
-      showModal: true,
-    });
-  };
+  const handleClickImage = useCallback((largeImageURL) => {
+    setLargeImageURL(largeImageURL);
+    setShowModal(true);
+  }, []);
 
-  render() {
-    const { images, showModal, largeImageURL, loader } = this.state;
+  // render() {
+  // const { images, showModal, largeImageURL, loader } = this.state;
 
-    return (
-      <div>
-        {loader && <Spiner />}
-        <Searchbar onSubmit={this.onSearchQuery} />
-        <ImageGallery images={images} onClickImage={this.handleClickImage} />
-        {images.length < 1 && (
-          <p className="notification">
-            {" "}
-            Please enter what you want to see ¯\_(ツ)_/¯{" "}
-          </p>
-        )}
-        {showModal && (
-          <Modal onClose={this.toggleModal}>
-            <img src={largeImageURL} alt="" width={650} />
-          </Modal>
-        )}
-        {images.length >= 12 ? <Button onSearch={this.fetchImages} /> : null}
-        <ToastContainer />
-      </div>
-    );
-  }
+  return (
+    <div>
+      {error && (
+        <p className="notification">
+          Please enter what you want to see ¯\_(ツ)_/¯{" "}
+        </p>
+      )}
+      {loader && <Spiner />}
+      <Searchbar onSubmit={onSearchQuery} />
+      <ImageGallery images={images} onClickImage={handleClickImage} />
+      {images.length < 1 && (
+        <p className="notification">
+          Please enter what you want to see ¯\_(ツ)_/¯{" "}
+        </p>
+      )}
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <img src={largeImageURL} alt="" width={650} />
+        </Modal>
+      )}
+      {images.length >= 12 ? <Button onSearch={nextPage} /> : null}
+      <ToastContainer />
+    </div>
+  );
 }
+// }
+
+// import React, { Component } from "react";
+// import Searchbar from "./components/Searchbar/Searchbar";
+// import ImageGallery from "./components/ImageGallery/ImageGallery";
+// import Spiner from "./components/Loader/Loader";
+// import Button from "./components/Button/Button";
+// import Modal from "./components/Modal/Modal";
+
+// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+// import { ToastContainer, toast } from "react-toastify";
+// import * as api from "./services/api";
+
+// import "./App.css";
+
+// export default class App extends Component {
+//   state = {
+//     images: [],
+//     query: "",
+//     page: 1,
+//     error: null,
+//     largeImageURL: "",
+//     showModal: false,
+//     loader: false,
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     const prevQuery = prevState.query;
+//     const nextQuery = this.state.query;
+
+//     if (prevQuery !== nextQuery) {
+//       this.fetchImages();
+//     }
+//     const prevPage = prevState.page;
+//     const nextPage = this.state.page;
+//     if (prevPage !== nextPage) {
+//       window.scrollTo({
+//         top: document.body.scrollHeight,
+//         behavior: "smooth",
+//       });
+//     }
+//   }
+//   onSearchQuery = (query) => {
+//     this.setState({
+//       images: [],
+//       query: query,
+//       page: 1,
+//     });
+//   };
+
+//   fetchImages = () => {
+//     const { query, page } = this.state;
+//     this.setState({ loader: true });
+
+//     api
+//       .fetchImages(query, page)
+//       .then(({ hits }) =>
+//         this.setState((prevState) => ({
+//           images: [...prevState.images, ...hits],
+//           page: prevState.page + 1,
+//         }))
+//       )
+
+//       .catch((error) => {
+//         toast("Trouble. Something is wrong :(");
+//         this.setState({ error });
+//       })
+
+//       .finally(() => this.setState({ loader: false }));
+//   };
+//   handleFormSubmit = (images) => {
+//     this.setState({ images });
+//   };
+
+//   toggleModal = () => {
+//     this.setState(({ showModal }) => ({
+//       showModal: !showModal,
+//       largeImageURL: "",
+//     }));
+//   };
+
+//   handleClickImage = (largeImageURL) => {
+//     this.setState({
+//       largeImageURL: largeImageURL,
+//       showModal: true,
+//     });
+//   };
+
+//   render() {
+//     const { images, showModal, largeImageURL, loader } = this.state;
+
+//     return (
+//       <div>
+//         {loader && <Spiner />}
+//         <Searchbar onSubmit={this.onSearchQuery} />
+//         <ImageGallery images={images} onClickImage={this.handleClickImage} />
+//         {images.length < 1 && (
+//           <p className="notification">
+//             {" "}
+//             Please enter what you want to see ¯\_(ツ)_/¯{" "}
+//           </p>
+//         )}
+//         {showModal && (
+//           <Modal onClose={this.toggleModal}>
+//             <img src={largeImageURL} alt="" width={650} />
+//           </Modal>
+//         )}
+//         {images.length >= 12 ? <Button onSearch={this.fetchImages} /> : null}
+//         <ToastContainer />
+//       </div>
+//     );
+//   }
+// }
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,30 +5,21 @@ import css from "./Modal.module.css";
 
 const modalRoot = document.querySelector("#modal-root");
 
-export default function Modal ({onClose}) {
-  // componentDidMount() {
-  //   window.addEventListener("keydown", this.handleKeyDown);
-  // }
-
-  // componentWillUnmount() {
-  //   window.removeEventListener("keydown", this.handleKeyDown);
-  // }
-
-  useEffect(() => {
-    window.addEventListener("keydown", handleKeyDown);
+export default function Modal({ onClose, children }) {
   
-    // returned function will be called on component unmount 
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.code === "Escape") {
+        onClose();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
-    }
-  }, [])
-
-
-  const handleKeyDown = (e) => {
-    if (e.code === "Escape") {
-      onClose();
-    }
-  };
+    };
+  }, [onClose]);
 
   const handleBackdropClick = (e) => {
     if (e.currentTarget === e.target) {
@@ -36,12 +27,10 @@ export default function Modal ({onClose}) {
     }
   };
 
-  // render() {
-    return createPortal(
-      <div className={css.overlay} onClick={handleBackdropClick}>
-        <div className={css.modal}>{this.props.children}</div>
-      </div>,
-      modalRoot
-    );
-  }
-// }
+  return createPortal(
+    <div className={css.overlay} onClick={handleBackdropClick}>
+      <div className={css.modal}>{children}</div>
+    </div>,
+    modalRoot
+  );
+}
